d3bdf2c2738fcbe7260096720be8a2ce60873ab0
Bhavya <xbhavyatomar@gmail.com>
2021-10-16 22:37:47 +0530

    remove unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import { createTheme, ThemeProvider } from "@mui/material";
 import { darkThemeOptions } from "./theme";
 import { CssBaseline } from "@mui/material";
@@ -8,23 +7,7 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <CssBaseline />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <CssBaseline />
     </ThemeProvider>
   );
 }
--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -3,5 +3,11 @@ import { ThemeOptions } from "@mui/material";
 export const darkThemeOptions: ThemeOptions = {
   palette: {
     mode: "dark",
+    primary: {
+      main: "#1DB954",
+      light: "rgb(74, 199, 118)",
+      dark: "rgb(20, 129, 58)",
+      contrastText: "rgba(0, 0, 0, 0.87)",
+    },
   },
 };
