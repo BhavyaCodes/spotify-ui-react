7dd54c5b5af9173b96da0e158ecbe818261127d9
Bhavya <xbhavyatomar@gmail.com>
2021-10-17 16:17:31 +0530

    add left and right buttons

---
+++ src/components/Layout-old.tsx
@@ -0,0 +1,97 @@
+// import AppBar from "@mui/material/AppBar";
+import { Paper } from "@mui/material";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Drawer from "@mui/material/Drawer";
+
+import Typography from "@mui/material/Typography";
+import { ReactNode, useState } from "react";
+import Leftbar from "components/Leftbar";
+
+const drawerWidth = 176;
+
+export default function ResponsiveDrawer({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const container = () => window.document.body;
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {/* {drawer} */}
+          <Leftbar />
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+          open
+        >
+          <Leftbar />
+          {/* {drawer} */}
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          pb: "3rem",
+        }}
+      >
+        <button onClick={handleDrawerToggle}>Open drawer</button>
+
+        {children}
+      </Box>
+      <Paper
+        sx={{
+          position: "fixed",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          zIndex: 10000,
+          height: "3rem",
+        }}
+      >
+        <Typography>bottom bar</Typography>
+      </Paper>
+    </Box>
+  );
+}
---
+++ src/components/Layout/TopNav/index.tsx
@@ -0,0 +1,51 @@
+import { Box, SvgIcon } from "@mui/material";
+import { ReactComponent as leftArrow } from "assets/icons/left-arrow.svg";
+import { ReactComponent as rightArrow } from "assets/icons/right-arrow.svg";
+
+export function TopNav() {
+  return (
+    <Box sx={{ display: "flex" }}>
+      <Box
+        sx={{
+          backgroundColor: "#0A0808",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          p: 1.5,
+          borderRadius: "50%",
+          fontSize: 15,
+          fontWeight: 900,
+          strokeWidth: 200,
+          mr: (theme) => theme.spacing(2),
+        }}
+      >
+        <SvgIcon
+          component={leftArrow}
+          viewBox="0 0 5 8"
+          fontSize="inherit"
+          sx={{ position: "relative", right: 1 }}
+        />
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: "#0A0808",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          p: 1.5,
+          borderRadius: "50%",
+          fontSize: 15,
+          fontWeight: 900,
+          strokeWidth: 200,
+        }}
+      >
+        <SvgIcon
+          component={rightArrow}
+          viewBox="0 0 5 8"
+          fontSize="inherit"
+          sx={{ position: "relative", left: 1 }}
+        />
+      </Box>
+    </Box>
+  );
+}
