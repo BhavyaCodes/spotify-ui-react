2fdb4e28b46aeed0a65dd057997be3efff66e9e6
Bhavya <xbhavyatomar@gmail.com>
2021-10-18 05:41:16 +0530

    add play button

--- src/pages/Artist.tsx
+++ src/pages/Artist.tsx
@@ -1,4 +1,5 @@
-// import { Box, SvgIcon, Typography } from "@mui/material";
+import { Box, SvgIcon } from "@mui/material";
+import { ReactComponent as PlayIcon } from "assets/icons/play-icon-white.svg";
 
 import { ArtistHeader } from "components/ArtistPage/ArtistHeader";
 
@@ -14,6 +15,27 @@ export type ArtistInfoType = {
 };
 
 export default function Artist({ artistInfo }: AppProps) {
-  const { name, image, listeners, verified } = artistInfo;
-  return <ArtistHeader artistInfo={artistInfo} />;
+  // const { name, image, listeners, verified } = artistInfo;
+  return (
+    <>
+      <ArtistHeader artistInfo={artistInfo} />
+      <Box sx={{ backgroundColor: "#191414", p: 2 }}>
+        <Box
+          p={2}
+          sx={{
+            backgroundColor: (theme) => theme.palette.primary.main,
+            borderRadius: "500px",
+          }}
+          component="span"
+          display="flex"
+          width={60}
+          height={60}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <SvgIcon component={PlayIcon} sx={{ ml: 0.5 }} viewBox="0 0 19 22" />
+        </Box>
+      </Box>
+    </>
+  );
 }
