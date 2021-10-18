9ec15e9106f481f7f663c7bc7157d2f5dea64373
Bhavya <xbhavyatomar@gmail.com>
2021-10-18 07:51:08 +0530

    remove comments from layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,7 +1,7 @@
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
 import Drawer from "@mui/material/Drawer";
-import { ReactNode, useState } from "react";
+import { ReactNode } from "react";
 import Leftbar from "components/Leftbar";
 import { TopNav } from "./TopNav";
 import BottomControls from "components/BottomControls";
@@ -14,61 +14,15 @@ const drawerWidth = 240;
 export default function Layout({ children }: { children: ReactNode }) {
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.down("md"));
-  const [mobileOpen, setMobileOpen] = useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
-
-  const container = () => window.document.body;
 
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
-
       <Box
         component="nav"
         sx={{ width: { lg: drawerWidth }, flexShrink: { lg: 0 } }}
         aria-label="mailbox folders"
       >
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-        {/* <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: "block", sm: "none" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
-        >
-          <Leftbar />
-        </Drawer> */}
-        {/* <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          anchor="right"
-          sx={{
-            display: { xs: "block", sm: "none" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
-        >
-          <Rightbar />
-        </Drawer> */}
         <Drawer
           variant="permanent"
           sx={{
@@ -95,7 +49,6 @@ export default function Layout({ children }: { children: ReactNode }) {
           anchor="right"
         >
           <Rightbar />
-          {/* {drawer} */}
         </Drawer>
       </Box>
       <Box
@@ -103,22 +56,15 @@ export default function Layout({ children }: { children: ReactNode }) {
         sx={{
           flexGrow: 0,
           p: 0,
-          // p: 3,
-          // width: { sm: "100%", md: "100%", lg: "100%" },
-
           width: { xl: `calc(100% - ${2 * drawerWidth}px)` },
           // width: { sm: `calc(100% - ${drawerWidth}px)` },
           pb: "3rem",
-          // backgroundColor: "#191414",
-          // overflowX: "auto",
-          // height: "auto",
           maxHeight: "100vh",
           overflowY: "scroll",
           position: "relative",
         }}
       >
         <TopNav />
-
         {children}
       </Box>
 
