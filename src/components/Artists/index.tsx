dd685d7b59124e58f94d933dff13ab6855d05571
Bhavya <xbhavyatomar@gmail.com>
2021-10-18 07:59:52 +0530

    fix grid for artists

--- src/components/Artists/index.tsx
+++ src/components/Artists/index.tsx
@@ -5,7 +5,7 @@ import { Artist } from "./Artist";
 export default function Artists({ artists }: { artists: IArtist[] }) {
   function renderArtists() {
     return artists.map((artist) => (
-      <Grid key={artist.name} item lg={2}>
+      <Grid key={artist.name} item lg={2} md={3} sm={4} xs={6}>
         <Artist {...artist} />
       </Grid>
     ));
